639dcaec01e9f89261dd2ea36534f69c7e35f036
ByeongHyeon <qudgus1984@naver.com>
2022-07-22 16:31:18 +0900

    Initial Commit

---
+++ SeSACTamagotchi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeSACTamagotchi
+//
+//  Created by 이병현 on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("잘 올라가는지 확인")
+
+    }
+
+}
+
